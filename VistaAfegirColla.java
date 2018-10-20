4ebfa435bbc5c74edca6631cc325e94a9308f768
aniolgamiz <sd00077@gmail.com>
2018-10-20 02:52:09 +0200

    Delete VistaAfegirColla.java

--- VistaAfegirColla.java
+++
@@ -1,9 +0,0 @@
-//aquesta vista ens permet afegir colles
-//té 2 botons
-//afegir més monitors (tornar enrere)
-//generar solució --> ens porta a la vista amb la solució
-package hackupc2018;
-
-public class VistaAfegirColla {
-    
-}
