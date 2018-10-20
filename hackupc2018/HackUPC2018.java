0764220b03f3ce0f173774b5ddd59f8439c6ccab
Donadeu <43960130+Donadeu@users.noreply.github.com>
2018-10-20 23:30:20 +0200

    Add files via upload

--- hackupc2018/HackUPC2018.java
+++ hackupc2018/HackUPC2018.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hackupc2018;
 
-/**
- *
- * @author Usuari
- */
 public class HackUPC2018 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        javax.swing.SwingUtilities.invokeLater (
+        new Runnable() {
+        public void run() {
+            ControladorPresentacio ctrlPresentacion = new ControladorPresentacio();
+            ctrlPresentacion.inicialitzarPresentacio();
+        }
+        });
+    }    
 }
