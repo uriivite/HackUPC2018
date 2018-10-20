8d2c9c7384cf5041fe259724112a9ddf7e680f51
aniolgamiz <sd00077@gmail.com>
2018-10-20 15:58:47 +0200

    vistes del programa

--- hackupc2018/ControladorPresentacio.java
+++ hackupc2018/ControladorPresentacio.java
@@ -6,6 +6,7 @@ public class ControladorPresentacio {
     private VistaPrincipal vistaP;
     private VistaAfegirMonitor vistaAM;
     private VistaAfegirColla vistaAC;
+    private VistaRestriccions vistaR;
     private VistaSolucio vistaS;
     
     // Fa falta que les vistes puguin fer actuar el Controldor de la Presentació.
@@ -14,15 +15,46 @@ public class ControladorPresentacio {
     
     public ControladorPresentacio() {
         ctrlDom = new ControladorDomini();
-        vistaP = new VistaPrincipal(this);
-        vistaAM = new VistaAfegirMonitor(this);
-        vistaAC = new VistaAfegirColla(this);
-        vistaS = new VistaSolucio(this);
+        vistaP = new VistaPrincipal();
+        vistaAM = new VistaAfegirMonitor();
+        vistaAC = new VistaAfegirColla();
+        vistaR = new VistaRestriccions();
+        vistaS = new VistaSolucio();
     }
     
     public void inicialitzarPresentacio() {
-        
+        vistaP.setVisible(true);
+        vistaP.ferVisible();
     }
+    
+    public void sincroVistaPAVistaAM() {
+        vistaP.setVisible(false);
+        vistaP.desactivar();
+        vistaAM.setVisible(true);
+        vistaAM.ferVisible();
+    }
+    
+    public void sincroVistaAMAVistaAC() {
+        vistaAM.setVisible(false);
+        vistaAM.desactivar();
+        vistaAC.setVisible(true);
+        vistaAC.ferVisible();
+    }
+    
+    public void sincroVistaACAVistaR() {
+        vistaAC.setVisible(false);
+        vistaAC.desactivar();
+        vistaR.setVisible(true);
+        vistaR.ferVisible();
+    }
+    
+    public void sincroVistaRAVistaS() {
+        vistaR.setVisible(false);
+        vistaR.desactivar();
+        vistaS.setVisible(true);
+        vistaS.ferVisible();
+    }
+    
     
     
     
