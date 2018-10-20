22c2d969d057f9ca2e35aeba26ba4d6cbcdc8dd1
oriolvidal <oriolvidalteruel@gmail.com>
2018-10-20 06:55:34 +0200

    He afegit algun comentari i arreglat algo de codi

--- hackupc2018/ControladorPresentacio.java
+++ hackupc2018/ControladorPresentacio.java
@@ -4,21 +4,23 @@ package hackupc2018;
 public class ControladorPresentacio {
     private ControladorDomini ctrlDom;
     private VistaPrincipal vistaP;
-    private VistaAfegirMonitor VistaAM;
-    private VistaAfegirColla VistaAC;
-    private VistaSolucio VistaS;
+    private VistaAfegirMonitor vistaAM;
+    private VistaAfegirColla vistaAC;
+    private VistaSolucio vistaS;
     
+    // Fa falta que les vistes puguin fer actuar el Controldor de la Presentació.
+    // Per això hi ha el (this), així a les classes de les vistes podem jugar
+    // amb l'objecte ControladorDomini.
     
     public ControladorPresentacio() {
         ctrlDom = new ControladorDomini();
-        vistaP = new VistaPrincipal();
-        VistaAM = new VistaAfegirMonitor();
-        VistaAC = new VistaAfegirColla();
-        VistaS = new VistaSolucio();
+        vistaP = new VistaPrincipal(this);
+        vistaAM = new VistaAfegirMonitor(this);
+        vistaAC = new VistaAfegirColla(this);
+        vistaS = new VistaSolucio(this);
     }
     
     public void inicialitzarPresentacio() {
-        ctrlDom.inicialitzarCtrlDomini();
         
     }
     
