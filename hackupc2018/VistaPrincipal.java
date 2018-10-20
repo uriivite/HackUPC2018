9ccc240b731fa96a4871ed43667cb46accc0aac0
oriolvidal <oriolvidalteruel@gmail.com>
2018-10-20 06:54:59 +0200

    He afegit algun comentari i arreglat algo de codi

--- hackupc2018/VistaPrincipal.java
+++ hackupc2018/VistaPrincipal.java
@@ -1,11 +1,8 @@
 package hackupc2018;
 //aquesta vista ha de ser la de benvinguda al programa
-//hi ha 1  botó
+//hi ha 1 botó
 //afegir monitors--> ens porta a la vista d'afegir monitors
 
-import java.awt.*;
-import javax.swing.*;
-
 public class VistaPrincipal {
-    
+    private ControladorPresentacio CP;
 }
