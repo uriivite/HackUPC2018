f54cc112f599ec181e3324c80433bc0895349fba
oriolvidal <oriolvidalteruel@gmail.com>
2018-10-20 06:58:04 +0200

    He afegit algun comentari i arreglat algo de codi

---
+++ hackupc2018/VistaRestriccions.java
@@ -0,0 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackupc2018;
+
+public class VistaRestriccions {
+    private ControladorPresentacio CP;
+}
